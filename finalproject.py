69627544386836586839d974148fb7d96ad1dfb2
Victoria <victoria.lopez@lawrence.edu>
2023-03-06 14:46:15 -0600

    Updated changes to final project draft

--- finalproject.py
+++ finalproject.py
@@ -1,31 +1,41 @@
-#Incomplete code. Random code everywhere, sorry if it doesn't make sense to you.
 import random
-#Test word. I want to use listofwords but don't know how to assign a number to each word for python to choose from.
-word = "religion" 
-#listofwords = ["familiar", "machine", "secretive", "waiting", "cushion", "hilarious", "invention", "damage", "natural", "scrawny", "wonder", "religion", "calculator", "tongue", "homeless", "daughter", "railway", "married", "dinosaur", "material"]
 
-print("Welcome to Hangman!\nYou will have 6 tries to try to guess the correct word.\nIf you don't guess the word correctly, you lose the game.")
+listofwords = ["familiar", "machine", "waiting", "cushion", "damage", "natural", "wonder", "religion", "tongue", "homeless", "daughter", "railway", "married", "dinosaur", "material"]
+word = random.choice(listofwords)
+print("\nWelcome to Hangman!\nYou will have 6 tries to try to guess the correct word.\nIf you don't guess the word correctly, you lose the game.")
 max = 6
 alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-letterguessed = ""
+letterguessed = []
 
-#Having problems with this loop. 
-while max > 0:
-    guess = input("Guess a letter: ")
-    max -= 1
-    if guess not in word:
-        print("Sorry, that letter is not in the word. Try again. " + str(max)+ " guesses left.")
-    if guess in word:
-        print("Good job! That letter is in the word. "+ str(max)+ " guesses left.")
-#    if guess not in word:
-#        print("Sorry, that letter is not in the word.")     
-    else :
-        guess not in alpha
+while max >= 2:
+    guess = input(" Guess a letter: ")
+    if guess not in alpha:
         print("That's not a letter, try again.")
+    elif guess not in word:
+        max -= 1
+        if max == 1: 
+            print("Sorry, that letter is not in the word. Try again. 1 guess left.")
+        else:
+            print("Sorry, that letter is not in the word. Try again. " + str(max)+ " guesses left.")
+    else:
+        guess in word
+        print("Good job! That letter is in the word.")
 
-#I don't know how to get correct letter in the dashes. Ex: if "i" is guessed, I want to print ___i_i__, but how would that work if python is choosing a word at random and almost all words have a different character count? 
     for letter in word:
-        if letter in letterguessed:
+        letterguessed.append(guess)
+        if letter in letterguessed: 
             print(letter, end="")
         else:
             print("_", end="")
+
+while max == 1:
+        yes_guess = input("\nYou have 1 guess left. Try to guess the word. ")
+        if yes_guess == word:
+            print ("\nCongragulations, you guessed the word correctly. You won the game!")
+            break
+        elif yes_guess != word:
+            print("\nSorry, you didn't guess the word correctly. You lost the game.\nThe real word was " + str(word) + "!")
+            break
+
+print("-------------------------------------------------------------------")
+#Last print statment inspired by Onyou Park
