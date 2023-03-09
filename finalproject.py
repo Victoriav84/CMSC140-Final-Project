ebfabbd17444d7f448a2267e772b5710e8f306a7
Victoria <victoria.lopez@lawrence.edu>
2023-03-09 09:58:59 -0600

    Updated Final Project

--- finalproject.py
+++ finalproject.py
@@ -6,8 +6,11 @@ print("\nWelcome to Hangman!\nYou will have 6 wrong tries to try to guess the co
 max = 6
 alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 letterguessed = []
-# How do I break the loop after the user guesses the word before the max is at 1?
+
 while max >= 2:
+    if set(letterguessed) == set(word):
+        print ("\nCongragulations, you guessed the word correctly. You won the game!")
+        break
     guess = input(" Guess a letter: ")
     if guess not in alpha:
         print("That's not a letter, try again.")
@@ -23,16 +26,20 @@ while max >= 2:
     
 
     for letter in word:
-        letterguessed.append(guess)
+        if guess in word:
+            letterguessed.append(guess)
         if letter in letterguessed: 
             print(letter, end="")
         else:
             print("_", end="")
 
 while max == 1:
+        if set(letterguessed) == set(word):
+            print ("\nCongragulations, you guessed the word correctly. You won the game!")
+            break
         yes_guess = input("\nYou have 1 guess left. Try to guess the word. ")
         if yes_guess == word:
-            print ("\nCongragulations, you guessed the word correctly. You won the game!")
+            print("\nCongragulations, you guessed the word correctly. You won the game!")
             break
         elif yes_guess != word:
             print("\nSorry, you didn't guess the word correctly. You lost the game.\nThe real word was " + str(word) + "!")
